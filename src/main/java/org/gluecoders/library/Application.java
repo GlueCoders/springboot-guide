88ec759fb49bfbc0b2f4aacf82b3548b11598b11
Anand Rajneesh <anandrajneesh@outlook.com>
2017-06-17 18:55:18 +0530

    [#10] war packaging

--- src/main/java/org/gluecoders/library/Application.java
+++ src/main/java/org/gluecoders/library/Application.java
@@ -2,12 +2,19 @@ package org.gluecoders.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by Anand_Rajneesh on 6/10/2017.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
