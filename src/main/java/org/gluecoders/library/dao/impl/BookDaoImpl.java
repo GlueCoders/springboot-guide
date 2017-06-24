d4f358e0626583cb4d4218b461ad74bde9249202
Anand Rajneesh <anand_rajneesh@infosys.com>
2017-06-24 12:07:33 +0530

    [#12] Configuration profiles

--- src/main/java/org/gluecoders/library/dao/impl/BookDaoImpl.java
+++ src/main/java/org/gluecoders/library/dao/impl/BookDaoImpl.java
@@ -1,13 +1,15 @@
 package org.gluecoders.library.dao.impl;
 
-import com.sun.org.apache.xml.internal.resolver.readers.TextCatalogReader;
 import org.gluecoders.library.dao.BookDaoCustom;
 import org.gluecoders.library.models.Book;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.*;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.TextCriteria;
+import org.springframework.data.mongodb.core.query.TextQuery;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
